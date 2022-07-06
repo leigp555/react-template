4db2fdb49e861847f69dafae8d34f6aff66ea803
leigp <907090585@qq.com>
2022-07-06 20:17:30 +0800

    feat: 路由配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import History from "./pages/History";
+
+import {
+    Routes,
+    Route,
+} from 'react-router-dom'
+
 function App() {
-  return (
-    <div className="App">
-      xxx
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path="/"  element={<Home/>}/>
+                <Route path="/history"  element={<History/>}/>
+                <Route path="/about"  element={<About/>}/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
