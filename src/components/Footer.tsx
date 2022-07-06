75077f91b5aab7d3f65ea43541c2ca3a52ee7331
leigp <907090585@qq.com>
2022-07-06 19:14:31 +0800

    feat: 引入路由

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <React.Fragment>
+            <h1>footer</h1>
+        </React.Fragment>
+    )
+}
+
+export default Footer
---
+++ src/pages/History.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function History(){
+    return(
+        <React.Fragment>
+            <h1>history</h1>
+        </React.Fragment>
+    )
+}
+
+export default History
